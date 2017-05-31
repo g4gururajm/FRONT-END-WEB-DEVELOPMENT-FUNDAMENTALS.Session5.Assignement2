e9ba5a24b9d750e809d8e6893cf8fc8b0357f1bc
g4gururajm <gururaj.tech@gmail.com>
2017-05-31 18:43:27 +0530

    Add files via upload

---
+++ app-obj.js
@@ -0,0 +1,29 @@
+		/* Creating new object*/
+		var myobj = new Object;
+		/* Variable to store name*/
+		myobj.name = "Smith";
+		/* Variable to store age*/
+		myobj.age  = 29;
+		/* Variable to store date of birth*/
+		myobj.dateofbirth = "01/01/1970";
+		/* Variable to store place of birth*/
+		myobj.placeofbirth = "USA, NewYork";
+		/* print on console that variable which store name*/
+		console.log("Name of the Person: " + myobj.name);
+		/* print on console that variable which store age*/
+		console.log("Age of the Person: " + myobj.age);
+		/* print on console that variable which store date of birth*/
+		console.log("Date of Birth: " + myobj.dateofbirth);
+		/* print on console that variable which store place of birth*/
+		console.log("Place of Birth: " + myobj.placeofbirth);
+		/* Creating customer object with variablle like name,age,Date of birth and birth place*/
+		 var customer = {'name' : 'SMITH','age':'29','DateofBirth':'01/01/1970','PlaceOfBirth':'USA,NewYork'};
+		 /* print on console that variable which customer name */
+		 console.log("Customer object name:  " + customer.name);
+		 /* print on console that variable which customer age*/
+		 console.log("Customer object age:  " + customer.age);
+		 /* print on console that variable which customer DateofBirthge*/
+		 console.log("Customer object DOB:  " + customer.DateofBirth);
+		 /* print on console that variable which customer PlaceOfBirth*/
+		 console.log("Customer object PlaceOfBirth:  " + customer.PlaceOfBirth);
+		
